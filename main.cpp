8d3b3816b98b490e9716171f3ca1914b3f5270fc
Eduardo Eugênio Kussler <eduardoeugeniok@gmail.com>
2021-03-20 00:57:27 -0300

    Adicionado a parte dada pelo professor e os arquivos pro cmake

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <opencv2/opencv.hpp>
+using namespace cv;
+int main(int argc, char** argv)
+{
+    int camera = 0;
+    VideoCapture cap;
+    // open the default camera, use something different from 0 otherwise;
+    // Check VideoCapture documentation. 
+    if (!cap.open(camera))
+        return 0;
+    while(true)
+    {
+        Mat frame1;
+        Mat frame;
+        cap >> frame;
+        cap >> frame1;
+        if (frame.empty()) break; // end of video stream
+        imshow("This is you, smile! :)", frame);
+        imshow("This is you, smile! :) - Filtered", frame1);
+        if (waitKey(1) == 27) break; // stop capturing by pressing ESC
+    }
+    cap.release();  // release the VideoCapture object
+    return 0;
+}
